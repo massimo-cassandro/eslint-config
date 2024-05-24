d3675b2ac708b685d11331960e8c46f6d70cf35a
Massimo Cassandro <massimo.cassandro@gmail.com>
2024-05-24 17:23:18 +0200

    v2: eslint 9

--- README.md
+++ README.md
@@ -7,36 +7,26 @@ Previous versions at <https://github.com/massimo-cassandro/linters-config>.
 
 ## Use
 
-Install `eslint` and this package:
+Install `eslint` and all the needed packages:
 
 ```bash
-npm install -D @massimo-cassandro/eslint-config eslint
+npm i -D eslint @eslint/js globals @massimo-cassandro/eslint-config
 ```
 
-Create a `.eslintrc.cjs` file (or use any other name and syntax you like, following the instructions at <https://eslint.org/docs/latest/use/configure/configuration-files>) and add:
+Create a `eslint.config.js` file (or use any other name and syntax you like, following the instructions at <https://eslint.org/docs/latest/use/configure/configuration-files>) and add:
 
 ```javascript
-/* eslint-env node */
+import eslint_config from '@massimo-cassandro/eslint-config';
 
-module.exports = {
-  extends: '@massimo-cassandro/eslint-config'
-};
+export default [
+  ...eslint_config,
+  {
+    files: ['src/**/*.js'],
+    ignores: [
+      'dist/',
+      '**/vendor/'
+    ],
+  }
+];
 ```
 
-## Use with React
-
-Add the [eslint-config-react-app](https://github.com/facebook/create-react-app/blob/main/packages/eslint-config-react-app/README.md) package:
-
-```bash
-npm install -D eslint-config-react-app
-```
-
-Add it to the `.eslintrc.cjs` file:
-
-```javascript
-/* eslint-env node */
-
-module.exports = {
-  extends: ['react-app', '@massimo-cassandro/eslint-config']
-};
-```
--- index.js
+++ index.js
@@ -1,58 +1,64 @@
-/* eslint-env node */
+import eslint_js from '@eslint/js';
+import globals from 'globals';
 
-module.exports = {
-  'env': {
-    'browser': true,
-    'jquery': true,
-    'es6': true
-  },
-  'extends': 'eslint:recommended',
-  'ignorePatterns': [
-    'dist/**/*.*',
-    '**/node_modules/**/*.*',
-    '**/vendor/**/*.*',
-    '**/*-min.js',
-    '**/*.min.js'
-  ],
-  'parserOptions': {
-    'ecmaVersion': 2023,
-    'sourceType': 'module'
-  },
-  'rules': {
-    'indent': [
-      'error',
-      2,
-      {
-        'SwitchCase': 1,
-        'ignoredNodes': ['TemplateLiteral *']
+
+export default [
+  eslint_js.configs.recommended,
+  {
+    name: '@massimo-cassandro/eslint-config',
+
+    languageOptions: {
+      ecmaVersion: 2023,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser
       }
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'warn',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'always'
-    ],
-    'eqeqeq': ['warn', 'always', {'null': 'ignore'}],
-    'no-extra-boolean-cast': 'off',
-    'no-extra-semi': 'error',
-    'no-throw-literal': 'off',
-    'semi-spacing': ['error', {
-      'before': false,
-      'after': true
-    }],
-    'no-console': 'warn',
-    'no-unused-vars': ['error', {
-      'vars': 'all',
-      'args': 'after-used',
-      'ignoreRestSiblings': false
-    }],
-    'import/no-anonymous-default-export': 0
-  }
-};
+    },
+
+    rules: {
+      'indent': [
+        'error',
+        2,
+        {
+          'SwitchCase': 1,
+          'ignoredNodes': ['TemplateLiteral *']
+        }
+      ],
+      'linebreak-style': [
+        'error',
+        'unix'
+      ],
+      'quotes': [
+        'warn',
+        'single'
+      ],
+      'semi': [
+        'error',
+        'always'
+      ],
+      'eqeqeq': ['warn', 'always', {'null': 'ignore'}],
+      'no-extra-boolean-cast': 'off',
+      'no-extra-semi': 'error',
+      'no-throw-literal': 'off',
+      'semi-spacing': ['error', {
+        'before': false,
+        'after': true
+      }],
+      'no-console': 'warn',
+      'no-unused-vars': ['error', {
+        'vars': 'all',
+        'args': 'after-used',
+        'ignoreRestSiblings': false
+      }],
+      'import/no-anonymous-default-export': 0
+    }
+  },
+  {
+    files: ['**/*.cjs'],
+    languageOptions: {
+      ecmaVersion: 2023,
+      sourceType: 'commonjs'
+    },
+  },
+
+];
