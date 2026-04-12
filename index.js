f8703d0a42a208834b11f8a2ba010e01cb8318d3
Massimo Cassandro <massimo.cassandro@gmail.com>
2026-04-12 18:58:34 +0200

    v.3.0.6 - Fix

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import globals from 'globals';
 export default [
   eslint_js.configs.recommended,
   {
+    files: ['**/*.js', '**/*.mjs', '**/*.jsx'],
     name: '@massimo-cassandro/eslint-config',
 
     languageOptions: {
